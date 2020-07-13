875cfaf8671b08ad29d060d4e8d0cf2a27585561
Maksim Karpets <maksimkarpets@mail.ru>
2020-07-13 12:09:51 +0300

    correct footer button submit

--- modal.js
+++ modal.js
@@ -2,13 +2,11 @@
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
-    submitModalBtn: document.querySelector("[data-modal-submit]"),
     modal: document.querySelector("[data-modal]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
-  refs.submitModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
